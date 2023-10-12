845f0c70635c3819430edf8cd7af8eb18e4a5915
sweekrutha <sweekruthass96@gmail.com>
2023-10-12 23:44:29 +0530

    Added Back end and front end code

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:knywpVTYMT9t9c2h@cluster0.kccg0e0.mongodb.net/bookStore?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
