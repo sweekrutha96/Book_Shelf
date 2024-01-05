e91888b2d5777f823b73ef0d8847faaa6f23a2ef
sweekrutha96 <119152370+sweekrutha96@users.noreply.github.com>
2024-01-05 23:30:07 +0530

    Update LoginService.js

--- Frontend/src/LoginService.js
+++ Frontend/src/LoginService.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const LoginService = data => (
-	axios.post('https://book-shelf-ispt.vercel.app/login', data)
-		.then(res => res.status)
-)
+	axios.post('https://book-shelf-ispt.vercel.app/login', data, {
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+}).then(res => res.status))
 
 export default LoginService;
