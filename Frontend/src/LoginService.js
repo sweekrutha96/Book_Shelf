dc410d0f558d33552f70b5e7c65c059830177f74
sweekrutha96 <119152370+sweekrutha96@users.noreply.github.com>
2024-01-05 23:52:49 +0530

    Update LoginService.js

--- Frontend/src/LoginService.js
+++ Frontend/src/LoginService.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const LoginService = data => (
 	axios.post('https://book-shelf-ispt.vercel.app/login', data, {
     headers: {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': 'https://book-shelf-eta-ruby.vercel.app',
+	    'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE'
     }
 }).then(res => res.status))
 
