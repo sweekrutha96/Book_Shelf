67896c3a7cebf38122ba5fa7737718e730d16715
sweekrutha96 <119152370+sweekrutha96@users.noreply.github.com>
2024-01-05 22:52:35 +0530

    Update LoginService.js

--- Frontend/src/LoginService.js
+++ Frontend/src/LoginService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const LoginService = data => (
-	axios.post('https://book-shelf-ispt-5bokssgiy-sweekruthas-projects.vercel.app/login', data)
+	axios.post('https://book-shelf-ispt.vercel.app/login', data)
 		.then(res => res.status)
 )
 
